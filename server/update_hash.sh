e29beb3b706f058519f678f568efe0995eb33a75
fechan <fred@fredchan.org>
2024-10-22 23:45:39 -0700

    add hash updater

---
+++ server/update_hash.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Updates the resource pack hash so that it matches the zip file in server.properties
+url=$(grep '^resource-pack=' server.properties | sed 's/resource-pack=//' | sed 's/\\:/:/') &&
+wget $url -O pack.zip &&
+
+hash=$(sha1sum < "pack.zip" | awk '{print $1}') &&
+echo $hash &&
+sed -i '/^resource-pack-sha1/ d' server.properties &&
+echo $'\n' >> server.properties &&
+echo "resource-pack-sha1=$hash" >> server.properties
