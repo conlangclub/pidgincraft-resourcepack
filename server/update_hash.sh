34e9f6c78d04c55a2d4ab49292e1a8b18e911d0d
fechan <fred@fredchan.org>
2024-10-22 23:48:43 -0700

    use bash and remove extra newline

--- server/update_hash.sh
+++ server/update_hash.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Updates the resource pack hash so that it matches the zip file in server.properties
 url=$(grep '^resource-pack=' server.properties | sed 's/resource-pack=//' | sed 's/\\:/:/') &&
@@ -7,5 +7,4 @@ wget $url -O pack.zip &&
 hash=$(sha1sum < "pack.zip" | awk '{print $1}') &&
 echo $hash &&
 sed -i '/^resource-pack-sha1/ d' server.properties &&
-echo $'\n' >> server.properties &&
 echo "resource-pack-sha1=$hash" >> server.properties
