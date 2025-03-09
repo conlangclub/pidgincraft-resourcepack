4b97456aa118f8043ac071583cb144106a072872
fechan <fred@fredchan.org>
2025-03-08 22:46:55 -0800

    change vocab sheet name

--- make_resourcepack.sh
+++ make_resourcepack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://docs.google.com/spreadsheets/d/1_SCZqJMZ1UrwqbQ69Bxpf0JZVwz93x3tBZFf4DPrEDM/gviz/tq\?tqx\=out:csv\&sheet\=pidgincraft-vocab -O vocab.csv &&
+wget https://docs.google.com/spreadsheets/d/1_SCZqJMZ1UrwqbQ69Bxpf0JZVwz93x3tBZFf4DPrEDM/gviz/tq\?tqx\=out:csv\&sheet\=Vocabulary\ Script -O vocab.csv &&
 wget https://docs.google.com/spreadsheets/d/1_SCZqJMZ1UrwqbQ69Bxpf0JZVwz93x3tBZFf4DPrEDM/gviz/tq\?tqx\=out:csv\&sheet\=Phrase\ Script -O phrases.csv &&
 python3 ./translate.py &&
 rm -f scc_resourcepack.zip &&
